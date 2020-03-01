6a1748f8f4b8026300586fecd4d14d764e38845e
isaac868 <33269071+isaac868@users.noreply.github.com>
2020-03-01 00:57:14 -0800

    testing websockets

--- src/bot.cpp
+++ src/bot.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include "atlstr.h"
+#include <cpprest\ws_msg.h>
 #include <cpprest\ws_client.h>
+
+using WSClient   = web::websockets::client::websocket_client;
+using WSOMessage = web::websockets::client::websocket_outgoing_message;
+using WSIMessage = web::websockets::client::websocket_incoming_message;
 
 int main()
 {
-    web::websockets::client::websocket_client test;
+    WSClient client;
 
+    client.connect(_T("wss://echo.websocket.org/")).wait();
 
-    std::cout << "test";
+    WSOMessage msg;
+    msg.set_utf8_message("test");
+    client.send(msg).then([]() { std::cout << "sending\n"; });
+
+    std::cout<<client.receive().then([](WSIMessage msg) {
+                        return msg.extract_string();
+                    }).get();
+
     return 0;
 }
